6567efbd222f7305aa980d456b537f387ed28671
Christine Callo <christinejoann.callo@gmail.com>
2017-06-23 13:46:10 -0700

    Added prepend method

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//Business Logic
+var number = function(year) {
+  return ("");
+}
+
+
+
+
+
+
+//User Interface
+$(document).ready(function() {
+  $("button#submit").click(function() {
+  //alert ("wow")
+  $("ul").prepend("<li>test</li>");
+  //console.log("");
+
+  event.preventDefault();
+  });
+});
