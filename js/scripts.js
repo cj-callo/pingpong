c5fa28e5747823d3500697a201e71f6b044ec07e
Christine Callo <christinejoann.callo@gmail.com>
2017-06-24 14:27:46 -0700

    business logic complete with required returns, though results not being displayed as list

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 //Business Logic
 var outcome = function(number) {
- if (number % 15 === 0)
-  return false
- if (number % 5 === 0) { //&& [number % 3 !== 0]) {
-   return true;
- } else (number % 3 === 0)
-    return false;
+  if (number % 15 === 0) {
+    return "pingpong";
+  } else if (number % 5 === 0) {
+    return "pong";
+  } else if (number % 3 === 0) {
+    return "ping"
+  } else {
+    return (number)
   }
-;
-
-
-
+};
 
 
 
@@ -22,8 +21,8 @@ var number = parseInt($("input#number").val());
   //console.log("");
   var result = outcome(number);
   //console.log("")
-  $("#result").text(result);
-  //$("li#result").prepend(result);
+  $("ul").append($("#result").text(result));
+//  $("#result").append("");
   //console.log("");
   event.preventDefault();
 });
